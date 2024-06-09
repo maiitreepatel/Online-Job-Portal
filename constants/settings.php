bd51458382e84851344a2ba9be59708accd88d46
maiitreepatel <149560793+maiitreepatel@users.noreply.github.com>
2024-06-09 22:27:17 +0530

    Add files via upload

---
+++ constants/settings.php
@@ -0,0 +1,31 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = '';
+$smtp_host = '';
+$smtp_user = '';
+$smtp_pass = '';
+
+$contact_mail = '';
+
+$fb  = '#';
+$tw = '#';
+$ig = '#';
+?>
+
+<?/*php
+// Set the default timezone
+date_default_timezone_set('Asia/Kolkata'); // Change 'Asia/Kolkata' to your preferred timezone
+
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$smtp_host = '';
+$smtp_user = '';
+$smtp_pass = '';
+
+$contact_mail = '';
+
+$fb  = '#';
+$tw = '#';
+$ig = '#';
+*/ ?>
